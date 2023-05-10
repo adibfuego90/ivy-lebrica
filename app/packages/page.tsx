bc3721ca7ea2257cc744d557972230d9bdd981fe
shohan-pherones <shohan.sub.56@gmail.com>
2023-05-10 14:40:00 +0600

    prisma model loaded, cva added, packages skeleton loaded

---
+++ app/packages/page.tsx
@@ -0,0 +1,33 @@
+import PackageItem from "@/components/PackageItem";
+import prisma from "@/libs/prisma";
+
+const PackagesPage = async () => {
+  const packages = await prisma.package.findMany({
+    include: {
+      specialists: true,
+    },
+  });
+
+  return (
+    <div className="wrapper py-10">
+      <h2>Packages</h2>
+
+      <div className="flex flex-wrap gap-10 mt-10">
+        {packages.map((pkg) => (
+          <PackageItem
+            key={pkg.id}
+            id={pkg.id}
+            image={pkg.image1}
+            title={pkg.title}
+            description={pkg.description}
+            tags={pkg.tags}
+            price={pkg.price}
+            specialists={pkg.specialists}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PackagesPage;
